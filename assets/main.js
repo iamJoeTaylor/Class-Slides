244480dbe5b41aa1db0aee28a2cad537b304ea4a
Joe Taylor <iamJoeTaylor@gmail.com>
2012-02-15 12:50:21 -0800

    pre stylin

--- assets/main.js
+++ assets/main.js
@@ -484,17 +484,64 @@ function updateHash() {
 
 function handleBodyKeyDown(event) {
   switch (event.keyCode) {
-    case 39:
-      nextSlide();
+    case 40: // down arrow
+      if (isChromeVoxActive()) {
+        speakNextItem();
+      } else {
+        nextSlide();
+      }
       event.preventDefault();
       break;
 
-    case 37:
-      prevSlide();
+    case 38: // up arrow
+      if (isChromeVoxActive()) {
+        speakPrevItem();
+      } else {
+        prevSlide();
+      }
       event.preventDefault();
       break;
   }
 };
+
+/*
+function handleBodyKeyDown(event) {
+  switch (event.keyCode) {
+    case 39: // right arrow
+    case 13: // Enter
+    case 32: // space
+    case 34: // PgDn
+      nextSlide();
+      event.preventDefault();
+      break;
+
+    case 37: // left arrow
+    case 8: // Backspace
+    case 33: // PgUp
+      prevSlide();
+      event.preventDefault();
+      break;
+
+    case 40: // down arrow
+      if (isChromeVoxActive()) {
+        speakNextItem();
+      } else {
+        nextSlide();
+      }
+      event.preventDefault();
+      break;
+
+    case 38: // up arrow
+      if (isChromeVoxActive()) {
+        speakPrevItem();
+      } else {
+        prevSlide();
+      }
+      event.preventDefault();
+      break;
+  }
+};
+*/
 
 function addEventListeners() {
   document.addEventListener('keydown', handleBodyKeyDown, false);  
